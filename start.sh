ed36c541122879158b9074c6576d78900dfa2827
bparrott78 <bparrott78@gmail.com>
2025-05-14 15:37:30 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,24 +7,27 @@ if [ -d "ComfyUI" ]; then
     cd ComfyUI
     if [ ! -d "venv" ]; then
         python3 -m venv venv
-        source venv/bin/activate
-        pip install --upgrade pip
-        pip install -r requirements.txt
-        pip install PyYAML
-    else
-        source venv/bin/activate
-        pip install --upgrade pip
-        pip install -r requirements.txt
-        pip install PyYAML
     fi
 else
     git clone https://github.com/comfyanonymous/ComfyUI.git
     cd ComfyUI
     python3 -m venv venv
-    source venv/bin/activate
-    pip install --upgrade pip
-    pip install -r requirements.txt
-    pip install PyYAML
 fi
 
+# Always activate venv and install/upgrade everything needed
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+pip install PyYAML
+
+echo "Python being used:"
+which python
+
+echo "Python version:"
+python --version
+
+echo "PyYAML version:"
+python -c "import yaml; print(yaml.__version__)"
+
+# Now run ComfyUI using *the venv Python*
 exec python main.py
